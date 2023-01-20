2c939017b79e260db255db484d21a85a3b46728d
max jn <maxjn763@gmail.com>
2023-01-20 14:11:44 +0330

    -Added DateBase

--- fithubdb.sql
+++ fithubdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 16, 2023 at 07:34 PM
+-- Generation Time: Jan 20, 2023 at 11:06 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -50,7 +50,7 @@ INSERT INTO `classes` (`classid`, `coachid`, `name`, `description`, `image`, `ti
 (2, 2, 'یوگا قدرتی', 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فسارا تکنولوژی مورد نیاز، و .', 'class-img3.jpg', '1 ماه', 12, 'شنبه, دوشنبه , سه شنبه', '8:00 - 9:00 ق.ظ', 30, 550),
 (3, 4, 'بدنسازی', 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فسارا تکنولوژی مورد نیاز، و .', 'class-img2.jpg', '1 ماه', 12, 'شنبه و پنجشنبه', '8:00 - 9:00 ق.ظ', 0, 400),
 (4, 3, 'تناسب اندام', 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فسارا تکنولوژی مورد نیاز، و .', 'class-img4.jpg', '1 ماه', 12, 'روز های فرد', '8:00 - 9:00 ق.ظ', 20, 300),
-(5, 4, 'تی آر ایکس', 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فسارا تکنولوژی مورد نیاز، و .', 'class-img5.jpg', '1 ماه', 12, 'روز های زوج', '8:00 - 9:00 ق.ظ', 30, 600),
+(5, 4, 'تی آر ایکس', 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فسارا تکنولوژی مورد نیاز، و .', 'class-img5.jpg', '1 ماه', 12, 'روز های زوج', '8:00 - 9:00 ق.ظ', 29, 600),
 (6, 6, 'ایروبیک', 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فسارا تکنولوژی مورد نیاز، و .', 'class-img6.jpg', '1 ماه', 12, 'روز های فرد', '8:00 - 9:00 ق.ظ', 30, 500);
 
 -- --------------------------------------------------------
@@ -82,6 +82,31 @@ INSERT INTO `coaches` (`coachid`, `name`, `image`, `expertice`, `phone`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `paymentid` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `number` varchar(16) NOT NULL,
+  `cvv` int(11) NOT NULL,
+  `expiredate` varchar(10) NOT NULL,
+  `money` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`paymentid`, `name`, `number`, `cvv`, `expiredate`, `money`) VALUES
+(1, 'پگاه اوجی فرد', '6219 8619 1128 4', 323, '07 / 08', 600),
+(2, 'پگاه اوجی فرد', '6219 8619 1128 4', 323, '07 / 08', 600),
+(3, 'پگاه اوجی فرد', '6219 8619 1128 4', 323, '07 / 08', 600),
+(4, 'پگاه اوجی فرد', '6219 8619 1128 4', 323, '07 / 08', 600);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `registers`
 --
 
@@ -89,8 +114,16 @@ CREATE TABLE `registers` (
   `registerid` int(11) NOT NULL,
   `classid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
+  `paymentid` int(11) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `registers`
+--
+
+INSERT INTO `registers` (`registerid`, `classid`, `userid`, `paymentid`, `date`) VALUES
+(5, 5, 1, 4, '2023-01-20');
 
 -- --------------------------------------------------------
 
@@ -133,12 +166,19 @@ ALTER TABLE `coaches`
   ADD PRIMARY KEY (`coachid`);
 
 --
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`paymentid`);
+
+--
 -- Indexes for table `registers`
 --
 ALTER TABLE `registers`
   ADD PRIMARY KEY (`registerid`),
   ADD KEY `register_classid_fk` (`classid`),
-  ADD KEY `register_userid_fk` (`userid`);
+  ADD KEY `register_userid_fk` (`userid`),
+  ADD KEY `register_paymentid_fk` (`paymentid`);
 
 --
 -- Indexes for table `users`
@@ -154,7 +194,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `classid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `classid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `coaches`
@@ -163,10 +203,16 @@ ALTER TABLE `coaches`
   MODIFY `coachid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `paymentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `registers`
 --
 ALTER TABLE `registers`
-  MODIFY `registerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `registerid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -189,6 +235,7 @@ ALTER TABLE `classes`
 --
 ALTER TABLE `registers`
   ADD CONSTRAINT `register_classid_fk` FOREIGN KEY (`classid`) REFERENCES `classes` (`classid`),
+  ADD CONSTRAINT `register_paymentid_fk` FOREIGN KEY (`paymentid`) REFERENCES `payments` (`paymentid`),
   ADD CONSTRAINT `register_userid_fk` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`);
 COMMIT;
 
